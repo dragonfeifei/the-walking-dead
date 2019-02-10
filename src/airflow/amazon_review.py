2ebcf312bfe33343baa960c661986e4f47f9ed6b
dragonfeifei <f.song8985@gmail.com>
2019-02-10 21:32:28 +0000

    interval

--- src/airflow/amazon_review.py
+++ src/airflow/amazon_review.py
@@ -14,12 +14,13 @@ default_args = {
     'start_date': dt.datetime(2019, 01, 01),
     'retries': 2,
     'retry_delay': dt.timedelta(minutes=1),
+    'depends_on_past': False,
 }
 
 
 with DAG('amazon_review',
          default_args=default_args,
-         schedule_interval=None,
+         schedule_interval='@daily',
          ) as dag:
 
     calc_review = BashOperator(task_id='calc_review',
